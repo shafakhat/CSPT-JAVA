61f7be3474aa7d7d1c9ce0da10151c4efc25ade2
Shafakhatullah Khan Mohammed <94445837+shafakhat@users.noreply.github.com>
2026-07-17 10:43:17 +0000

    5yiretpooyirtopy

---
+++ CSPT1-170726/Prog1.java
@@ -0,0 +1,11 @@
+void main(){
+	int x = 0;
+	boolean b = true;
+	boolean  f = false;
+	IO.println(b + " " + x + " "+f);
+
+	char ch = '3';
+	
+    int y = Integer.parseInt(String.valueOf(ch));
+    IO.println(y);
+}
